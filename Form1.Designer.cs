c9e6da4261dacef138812356c6317b88a0517163
jasherton <jjelamb@gmail.com>
2019-01-17 17:42:25 -0600

    files

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace KSA_ModelImporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileDia = new System.Windows.Forms.OpenFileDialog();
+            this.LoadedName = new System.Windows.Forms.Label();
+            this.SaveFileDia = new System.Windows.Forms.SaveFileDialog();
+            this.creditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DimGray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenu,
+            this.creditToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileMenu
+            // 
+            this.FileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFileStrip,
+            this.saveToolStripMenuItem});
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new System.Drawing.Size(37, 20);
+            this.FileMenu.Text = "File";
+            // 
+            // OpenFileStrip
+            // 
+            this.OpenFileStrip.Name = "OpenFileStrip";
+            this.OpenFileStrip.Size = new System.Drawing.Size(152, 22);
+            this.OpenFileStrip.Text = "Open";
+            this.OpenFileStrip.Click += new System.EventHandler(this.OpenFileStrip_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Visible = false;
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // OpenFileDia
+            // 
+            this.OpenFileDia.Filter = "Model File|*.bfres; *.cmp";
+            // 
+            // LoadedName
+            // 
+            this.LoadedName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadedName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadedName.ForeColor = System.Drawing.Color.White;
+            this.LoadedName.Location = new System.Drawing.Point(712, 2);
+            this.LoadedName.Name = "LoadedName";
+            this.LoadedName.Size = new System.Drawing.Size(70, 20);
+            this.LoadedName.TabIndex = 1;
+            this.LoadedName.Text = "None";
+            this.LoadedName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaveFileDia
+            // 
+            this.SaveFileDia.FileName = "a";
+            this.SaveFileDia.Filter = "Compressed Model File|*.cmp";
+            // 
+            // creditToolStripMenuItem
+            // 
+            this.creditToolStripMenuItem.Name = "creditToolStripMenuItem";
+            this.creditToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.creditToolStripMenuItem.Text = "Credits";
+            this.creditToolStripMenuItem.Click += new System.EventHandler(this.creditToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.LoadedName);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Kirby Star Allies Model Importer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileMenu;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileStrip;
+        private System.Windows.Forms.OpenFileDialog OpenFileDia;
+        private System.Windows.Forms.Label LoadedName;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog SaveFileDia;
+        private System.Windows.Forms.ToolStripMenuItem creditToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Reflection;
+
+namespace KSA_ModelImporter
+{
+    public partial class Form1 : Form
+    {
+        Utility.BFRES model;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void LoadBFRES(string fname)
+        {
+            model = new Utility.BFRES();
+            model.Evaluate(fname);
+
+            if (model.Read())
+            {
+                LoadedName.Text = Path.GetFileNameWithoutExtension(fname);
+                saveToolStripMenuItem.Visible = true;
+                //MessageBox.Show("File Validated.", "BFRES File", MessageBoxButtons.OK);
+            }
+        }
+
+        private void OpenFileStrip_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDia.ShowDialog() == DialogResult.OK)
+            {
+                var fname = OpenFileDia.FileName;
+                var nwext = Path.GetFileNameWithoutExtension(OpenFileDia.FileName);
+                var ext = Path.GetExtension(OpenFileDia.FileName);
+                var dir = Path.GetDirectoryName(OpenFileDia.FileName);
+                if (ext == ".bfres")
+                {
+                    if (model != null) { model.Exit(); }
+                    LoadBFRES(OpenFileDia.FileName);
+                }
+                else
+                if (ext == ".cmp")
+                {
+                    bool allow = true;
+
+                    if (model != null)
+                    {
+                        if (nwext == Path.GetFileName(model.FileName))
+                        {
+                            //oh boy this looks bad let's not do this
+                            allow = false;
+                            MessageBox.Show("File is currently loaded, cannot decompress.", "CMP File", MessageBoxButtons.OK);
+                        }
+                    }
+
+                    if (allow)
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        startInfo.Arguments = "/C cd " + Directory.GetCurrentDirectory() + "/tools" + "&" + " quickbms -o KirbyStarAllies-Decompress.bms " + fname + " " + dir;
+
+
+                        using (var Proc = Process.Start(startInfo))
+                        {
+                            Proc.WaitForExit();
+                        }
+
+                        var resp = MessageBox.Show("File decompressed. Want to load the file?", "CMP File", MessageBoxButtons.YesNo);
+
+                        if (resp == DialogResult.Yes)
+                        {
+                            if (model != null) { model.Exit(); }
+                            LoadBFRES(dir + "/" + nwext);
+                        }
+                    }
+
+                }else
+                {
+                    MessageBox.Show("The file you selected is not .bfres or .cmp", "Invalid File Type", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (model != null)
+            {
+                if (model.FileOk)
+                {
+                    model.Exit();
+                }
+            }
+            return;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDia.InitialDirectory = Path.GetDirectoryName(OpenFileDia.FileName);
+            SaveFileDia.FileName = Path.GetFileName(OpenFileDia.FileName);
+            if (SaveFileDia.ShowDialog() == DialogResult.OK)
+            {
+                if (SaveFileDia.FileName != null && SaveFileDia.FileName != "")
+                {
+                    //save file
+                    if (File.Exists(SaveFileDia.FileName)) { File.Delete(SaveFileDia.FileName); };
+                    bool IsSaved = model.SaveTo(SaveFileDia.FileName);
+                    if (IsSaved)
+                    {
+                        return;
+                    }   
+                    
+                }
+            }
+        }
+
+        private void creditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+@"
+Grand Dad - Tool, CMP Compressor
+
+RandomTBush - CMP Decompressor
+
+Luigi Auriemma - QuickBMS
+", "Credits", MessageBoxButtons.OK);
+        }
+    }
+}
